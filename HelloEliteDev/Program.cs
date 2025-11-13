3da587025b61f0f6708513bb1dd200763eb959be
LordMavaia <roble.mavaia@gmail.com>
2025-11-12 21:24:37 -0600

    Dia 1: primer proyecto

--- HelloEliteDev/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Este es mi primer dia");
-
-string nombre ="Jorge Omar";
-int edad = 35;
-bool activo = true;
-
-Console.WriteLine($"My name is {nombre}, I am {edad} years old, and learning: {activo}");
--- README.md.txt
+++
@@ -1,3 +0,0 @@
-This is my fists day learning Git
-
-I become to dev profesional, I be goint to best 
